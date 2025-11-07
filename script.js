fe0fc0d96c94f59aad10b5e01e56cb5ce6273eee
Adriano <adrianosenai604@gmail.com>
2025-11-07 19:37:10 -0300

    feat(api): atualizar rota base da api

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 async function chamarAPI() {
-  const resposta = await fetch("https://projeto-simples-back.onrender.com/")
+  const resposta = await fetch("https://projeto-simples-back2.onrender.com/")
   const dados = await resposta.json()
   document.getElementById("resposta").textContent = dados.message
 }
